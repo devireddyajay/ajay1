eb98735cc74e97163e302c6b7026ba8c6c82098b
devireddy ajay <190030348@kluniversity.in>
2024-02-13 00:18:08 -0600

    Created sum java file to calculate sum of two numbers

--- sum.java
+++ sum.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class sum {
+public class sum {
+    public static void main(String args[]) {
+
+
+        int a = 10;
+        int b = 4;
+        int c= a+b;
+        System.out.println("sum is"+ c);
+    }
+
 }
